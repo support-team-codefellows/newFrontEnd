80133f11cdf3b417d871bb031c4222ba5a464360
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2022-01-05 12:03:34 +0200

    what shows to who and homepage

---
+++ components/welcome.js
@@ -0,0 +1,152 @@
+import { useContext } from "react";
+import { LoginContext } from "./auth/context";
+import {
+  Button,
+  Flex,
+  Link,
+  Image,
+  Heading,
+  Stack,
+  Text,
+  Stat,
+  StatLabel,
+  Box,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { If, Then, Else } from "react-if";
+
+export default function Hero() {
+  const Context = useContext(LoginContext);
+  return (
+    <>
+      <Stack
+        spacing={4}
+        w={{ base: "80%", md: "40%" }}
+        align={["center", "center", "flex-start", "flex-start"]}
+      >
+        <Heading
+          as="h1"
+          size="xl"
+          color="primary.800"
+          fontWeight="normal"
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Welcome Back, <b>{Context.user.username}</b>
+        </Heading>
+        <Text fontSize="small" color="gray" ml={1}>
+          {new Date().toLocaleDateString(undefined, {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            weekday: "short",
+          })}
+        </Text>
+
+        <If condition={Context.user?.capabilities?.length > 2}>
+          <Then>
+            <br />
+            <br />
+            <SimpleGrid
+              columns={{ base: 1, md: 2 }}
+              spacing={{ base: 5, lg: 8 }}
+            >
+              <Link href="/telephone">
+                <Stat
+                  px={{ base: 2, md: 0 }}
+                  py={"5"}
+                  shadow={"xl"}
+                  border={"1px solid"}
+                  borderColor={("gray.800", "gray.500")}
+                  rounded={"lg"}
+                >
+                  <Flex justifyContent={"space-between"}>
+                    <Box pl={{ base: 2, md: 4 }} pr={{ base: 2, md: 4 }}>
+                      <StatLabel fontWeight={"medium"} isTruncated>
+                        <Text fontSize="md" fontWeight="bold">
+                          Telephone <br /> Department
+                        </Text>
+                      </StatLabel>
+                    </Box>
+                  </Flex>
+                </Stat>
+              </Link>{" "}
+              <Link href="/site">
+                <Stat
+                  px={{ base: 2, md: 0 }}
+                  py={"5"}
+                  shadow={"xl"}
+                  border={"1px solid"}
+                  borderColor={("gray.800", "gray.500")}
+                  rounded={"lg"}
+                >
+                  <Flex justifyContent={"space-between"}>
+                    <Box pl={{ base: 2, md: 4 }} pr={{ base: 2, md: 4 }}>
+                      <StatLabel fontWeight={"medium"} isTruncated>
+                        <Text fontSize="md" fontWeight="bold">
+                          On-Site <br /> Department
+                        </Text>
+                      </StatLabel>
+                    </Box>
+                  </Flex>
+                </Stat>
+              </Link>
+            </SimpleGrid>
+          </Then>
+
+          <Else>
+            <br />
+            <br />
+            <SimpleGrid
+              columns={{ base: 1, md: 2 }}
+              spacing={{ base: 5, lg: 8 }}
+            >
+              <Link href="/customer">
+                <Stat
+                  px={{ base: 2, md: 0 }}
+                  py={"5"}
+                  shadow={"xl"}
+                  border={"1px solid"}
+                  borderColor={("gray.800", "gray.500")}
+                  rounded={"lg"}
+                >
+                  <Flex justifyContent={"space-between"}>
+                    <Box pl={{ base: 2, md: 4 }} pr={{ base: 2, md: 4 }}>
+                      <StatLabel fontWeight={"medium"} isTruncated>
+                        <Text fontSize="md" fontWeight="bold">
+                          Submit a Ticket
+                        </Text>
+                      </StatLabel>
+                    </Box>
+                  </Flex>
+                </Stat>
+              </Link>{" "}
+            </SimpleGrid>
+          </Else>
+        </If>
+        <br />
+        <br />
+        <Button
+          colorScheme="pink"
+          bgColor="#b57295"
+          borderRadius="8px"
+          py="4"
+          px="4"
+          lineHeight="1"
+          size="md"
+          onClick={Context.logout}
+        >
+          Logout
+        </Button>
+      </Stack>
+      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
+        <Image
+          src="https://7dfairs.com/web/wp-content/uploads/2021/03/shutterstock_1753698680.jpg"
+          //   src="https://y26uq11r8xr1zyp0d3inciqv-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/41-1-1024x597.jpg"
+          alt="hero image"
+          size="100%"
+          objectFit="cover"
+        />
+      </Box>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ import {
   Link,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
-import ChatFrom from "../components/chat/ChatForm";
+import Landing from "../components/landing";
+import Welcome from "../components/welcome";
 import { If, Then, Else } from "react-if";
 
 export default function Home() {
- 
   const Context = useContext(LoginContext);
+  console.log("here ->>", Context);
   return (
     <>
-
       <Flex
         w={["100%", "100%", "90%", "90%", "85%"]}
         p="3%"
@@ -36,99 +36,16 @@ export default function Home() {
           px={8}
           mb={16}
         >
-          <Stack
-            spacing={4}
-            w={{ base: "80%", md: "40%" }}
-            align={["center", "center", "flex-start", "flex-start"]}
-          >
-            <Heading
-              as="h1"
-              size="xl"
-              fontWeight="bold"
-              color="primary.800"
-              textAlign={["center", "center", "left", "left"]}
-            >
-              Tangled
-            </Heading>
-            <Heading
-              as="h2"
-              size="md"
-              color="primary.800"
-              opacity="0.8"
-              fontSize="16px"
-              fontWeight="normal"
-              lineHeight={1.5}
-              textAlign={["center", "center", "left", "left"]}
-            >
-              Customers value time and convenience, and while you can’t always
-              physically meet your customers where they’re at, you can use
-              omni-channel feedback and multiple customer support options to
-              allow customers to contact your support team in the way that’s
-              most convenient for them.
-              <br />
-              <br />
-              Tangled is a customer service platform that offers quality, fast,
-              and reliable customer service for your business.
-            </Heading>
-            {Context.logout ? (
-              <span>
-                <Link href="/signup">
-                  <Button
-                    colorScheme="pink"
-                    bgColor="#b57295"
-                    borderRadius="8px"
-                    py="4"
-                    px="4"
-                    lineHeight="1"
-                    size="md"
-                  >
-                    Signup
-                  </Button>{" "}
-                </Link>
-                <Link href="/login">
-                  <Button
-                    colorScheme="pink"
-                    bgColor="#b57295"
-                    borderRadius="8px"
-                    py="4"
-                    px="4"
-                    lineHeight="1"
-                    size="md"
-                  >
-                    Login
-                  </Button>
-                </Link>
-              </span>
-            ) : (
-              <></>
-            )}
-          </Stack>
-          <Box
-            w={{ base: "80%", sm: "60%", md: "50%" }}
-            mb={{ base: 12, md: 0 }}
-          >
-            {/* TODO: Make this change every X secs */}
-            <Image
-              src="https://7dfairs.com/web/wp-content/uploads/2021/03/shutterstock_1753698680.jpg"
-              //   src="https://y26uq11r8xr1zyp0d3inciqv-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/41-1-1024x597.jpg"
-              alt="hero image"
-              size="100%"
-              objectFit="cover"
-            />
-          </Box>
+          <If condition={Context.loggedIn}>
+            <Then>
+              <Welcome />
+            </Then>
+            <Else>
+              <Landing />
+            </Else>
+          </If>
         </Flex>
       </Flex>
-      {/* <Text
-                  fontSize="xs"
-                  mt={2}
-                  textAlign="center"
-                  color="primary.800"
-                  opacity="0.6"
-                >
-                  No credit card required.
-                </Text>{" "} */}
     </>
   );
 }
-
-
